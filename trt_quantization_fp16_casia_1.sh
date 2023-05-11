9afb2b9f048e314b42068716f3c8a9a8f56d8cd6
Thadli Guerra <130626268+tatatadlisixti4@users.noreply.github.com>
2023-05-11 12:47:11 -0400

    Delete trt_quantization_fp16_casia_1.sh

--- trt_quantization_fp16_casia_1.sh
+++
@@ -1,2 +0,0 @@
-/usr/src/tensorrt/bin/trtexec --onnx=/home/rhernandez/onnx_models/casia.onnx --saveEngine=/home/rhernandez/modelos_trt/casia_batch1_fp16.trt --fp16
-/usr/src/tensorrt/bin/trtexec --loadEngine=/home/rhernandez/modelos_trt/casia_batch1_fp16.trt > casia_batch1_fp16.txt
