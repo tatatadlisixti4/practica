e925289250e0387c9d0e34b93c0e33fd68934763
Thadli Guerra <130626268+tatatadlisixti4@users.noreply.github.com>
2023-05-18 22:20:25 -0400

    Add files via upload

--- código_maestroV2.py
+++ código_maestroV2.py
@@ -5,6 +5,7 @@ import cv2
 import argparse
 import numpy as np
 import time
+from tensorflow import keras
 from keras.utils import to_categorical
 import tensorrt as trt
 from TensorRT import common
